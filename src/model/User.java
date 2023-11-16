720b135f2f62ac9d05b2b0c428eedcc2169f6984
Wajid Hussain <52674797+wajidengg@users.noreply.github.com>
2023-11-16 00:09:47 -0500

    Previous commented code removed

---
+++ src/model/User.java
@@ -0,0 +1,104 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author Wajid
+ */
+public class User {
+    String fname;
+    String lname;
+    int age;
+    String email;
+    String message;
+    String uploadImage;
+    String gender;
+    String type;
+    String date;
+    int id;
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getUploadImage() {
+        return uploadImage;
+    }
+
+    public void setUploadImage(String uploadImage) {
+        this.uploadImage = uploadImage;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+}
---
+++ src/util/DatabaseConnector.java
@@ -0,0 +1,120 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package util;
+
+import java.sql.*;
+import java.util.ArrayList;
+import model.User;
+
+/**
+ * Database Connector class for interacting with database
+ * @author akshatr
+ */
+public class DatabaseConnector {
+
+    private static final String URL = "jdbc:mysql://localhost:3306/test?useSSL=false";
+    private static final String USERNAME = "root";
+    private static final String PASSWORD = "1";
+
+    /**
+     * Privatized constructor so as to not allow object creation
+     */
+    private DatabaseConnector() {
+    }
+
+    /**
+     * Insert given user to database
+     * @see User
+     * @param user User object to be added
+     */
+    public static void addUser(User user) {
+        //add to database
+        String query = "INSERT INTO USER(fname, lname, age, gender, email, date) VALUES(?,?,?,?,?,?)";
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, user.getFname());
+            stmt.setString(2, user.getLname());
+            stmt.setInt(3, user.getAge());
+            stmt.setString(4, user.getGender());
+            stmt.setString(5, user.getEmail());
+            stmt.setString(6, user.getDate());
+            int rows = stmt.executeUpdate();
+            System.out.println("Rows impacted : " + rows);
+//            conn.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Return lost of all users in database
+     * @see User
+     * @return list of users
+     */
+    public static ArrayList<User> getAllusers() {
+//        return list of users from db
+        ArrayList<User> users = new ArrayList<>();
+
+        String query = "SELECT * FROM USER";
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(query);
+            while (rs.next()) {
+                User u = new User();
+                u.setFname(rs.getString("fname"));
+                u.setLname(rs.getString("lname"));
+                u.setAge(rs.getInt("age"));
+                u.setGender(rs.getString("gender"));
+                u.setEmail(rs.getString("email"));
+                u.setDate(rs.getString("date"));
+                u.setId(rs.getInt("id"));
+                users.add(u);
+            }
+            rs.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
+        return users;
+    }
+
+    /**
+     * Delete given user from database
+     * @see User
+     * @param u User to be deleted
+     * 
+     */
+    public static void deleteUser(User u) {
+        String query = "delete from USER where id = ?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setInt(1, u.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * Edit given user details in the database
+     * @param oldUser existing user in database
+     * @param newUser modified user details to be added
+     */
+    public static void editUser(User oldUser, User newUser) {
+        String query = "UPDATE USER SET name=?, age=? WHERE id=?";
+
+        try (Connection conn = DriverManager.getConnection(URL, USERNAME, PASSWORD)) {
+            PreparedStatement stmt = conn.prepareStatement(query);
+            stmt.setString(1, newUser.getFname());
+            stmt.setInt(2, newUser.getAge());
+            stmt.setInt(3, oldUser.getId());
+            stmt.executeUpdate();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+}
